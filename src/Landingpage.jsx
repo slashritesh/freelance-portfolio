3e179fc41ee761a8e2e7d431d4ef90d2b2acc8a8
Ritesh Mane <maneriteshh@gmail.com>
2023-07-01 21:37:01 +0530

    updating profile image

--- src/Landingpage.jsx
+++ src/Landingpage.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import logo from './assets/logo.svg'
-import profile from './assets/profile-img.jpeg'
+import profile from './assets/Design.png'
 import work1 from './assets/work-1.png'
 import work2 from './assets/work2.png';
 import heroimg from './assets/workspace.png';
@@ -28,7 +28,7 @@ const Landingpage = () => {
             <button className='px-8 text-lg rounded-xl py-4 bg-blue-600'>Work with me</button>
           </div>
           <div className='flex justify-end flex-1'>
-            <img className='rounded-xl sm:mt-10 w-[900px] sm:w-[500px]' src={heroimg} alt="Ritesh mane" />
+            <img className='rounded-xl sm:mt-10 w-[900px] sm:w-full ' src={heroimg} alt="Ritesh mane" />
 
           </div>
         </div>
@@ -40,10 +40,9 @@ const Landingpage = () => {
 
       <div className='py-2 Boxed'>
         <hr />
-        <div className='flex sm:flex-col justify-between p-10'>
+        <div className='flex sm:flex-wrap justify-between p-5'>
           <p className='text-slate-600'>Slash Ritesh</p>
           <p className='text-slate-600'>Privacy policy</p>
-          <p className='text-slate-600'>maneriteshh@gmail.com</p>
         </div>
       </div>
     </>
@@ -62,7 +61,7 @@ const About = () => {
           </div>
           <div className='flex-1 pr-10 sm:p-0'>
             <i className=' text-slate-500'>
-              "My name is Ritesh from India. I am working on this field last 2yr and also work with the co-opreate and freelance project <br />
+              "My name is Ritesh from India. I am working in this field last 2yr and also work with the co-opreate and freelance project <br />
               <br />
               my aim is to help bussiness to build there strong online presence with not only with website with also applying the strategies that actually work for them and help to achive massive result with our services
               <br />
@@ -73,7 +72,7 @@ const About = () => {
               <br />
               <br />
               but actually placing the images , design and writting better copy matters when we saw website as bussiness growth prespective .
-              <br /><br />also adding the automation and systems integartion which help bussiness to be productive and delivery services seamlessly
+              <br /><br />also adding the automation and systems integartion which help bussiness to be productive and deliver services seamlessly
               "
               </div>
             </i>
@@ -93,11 +92,13 @@ const About = () => {
 const CalltoAction =()=>{
   return(
     <>
-    <div className='Boxed py-2 pb-20 '>
-      <div className='bg-blue-800 sm:h-96 bg-opacity-20 relative border border-blue-600 flex flex-col gap-5 rounded-2xl p-10'>
-        <h1 className='p-2 bg-gray-600 w-fit sm:text-sm rounded-full px-10'>Hurry up! Its time to Be professional</h1>
-        <h1 className='text-4xl font-semibold sm:text-3xl '>Book a Call to get offer in Best Budget</h1>
-        <button className='px-10 py-4 rounded-2xl absolute bottom-10 right-20 bg-indigo-600'> Book a call </button>
+    <div className='Boxed py-2 pb-20 sm:pb-10 '>
+      <div className='bg-blue-800 sm:h-fit bg-opacity-20 relative border border-blue-600 flex flex-col gap-5 rounded-2xl p-10'>
+        <h1 className='p-2 bg-gray-600 w-fit sm:hidden rounded-full px-10'>Build online presence professional</h1>
+        <div className='flex sm:flex-col justify-between items-center'>
+        <h1 className='text-4xl  font-semibold sm:text-2xl sm:mb-10 '>Book a Call to get offer in Best Budget</h1>
+        <button className='px-10 py-4 sm:p-3 rounded-2xl bg-indigo-600'> Connect into DM </button>
+        </div>
       </div>
     </div>
     </>
@@ -110,10 +111,10 @@ const Work = () => {
     <>
       <div className='Boxed py-40 sm:py-36'>
         <h1 className='text-center text-4xl sm:text-3xl font-semibold'>My Recent Work</h1>
-        <div className='text-center p-5'>
-          <p>✅ Professional design  &nbsp; &nbsp;</p>
-          <p>✅ High converting   &nbsp; &nbsp;</p>
-          <p>✅ Lead Capturing stategies</p>
+        <div className='text-center justify-center gap-5 flex sm:flex-col p-5'>
+          <p className='p-2 px-5 sm:text-sm bg-blue-950 rounded-full border border-blue-600'>✅ Professional design  &nbsp; &nbsp;</p>
+          <p className='p-2 px-5 sm:text-sm bg-blue-950 rounded-full border border-blue-600'>✅ High converting   &nbsp; &nbsp;</p>
+          <p className='p-2 px-5 sm:text-sm bg-blue-950 rounded-full border border-blue-600'>✅ Proven stategies</p>
         </div>
 
         <div className='grid grid-cols-2 sm:grid-cols-1 gap-10 pt-10'>
